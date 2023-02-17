cbca04dc199a8e8bfea4649764b4edaa689a1712
QodirovDoston <qodirovdoston806@gmail.com>
2023-02-17 22:10:14 +0500

    fdSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navabrmenyu from "./companents/Navbar/Navabrmenyu";
-import {  Routes, Router ,Route } from "react-router-dom";
+import {  Routes ,Route } from "react-router-dom";
 import Home from "./companents/Home/Home";
 import About from "./companents/About/About";
 import NotFound from "./companents/NotFound/NotFound";
@@ -8,12 +8,15 @@ import NotFound from "./companents/NotFound/NotFound";
 function App() {
   return (
   <>
+  {/* <Router> */}
+
     <Navabrmenyu/>
    <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/about" element={<About />} />
     <Route path="/erorr" element={<NotFound/>} />
   </Routes> 
+  {/* </Router> */}
     </>
   );
 }
